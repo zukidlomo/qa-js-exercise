247fb3491b94205da331bf5ae559e1336bffc80c
zukidlomo <zdlomo023@student.wethinkcode.co.za>
2025-05-21 20:10:43 +0200

    Completed exercise 2

--- exercises/2_strings.js
+++ exercises/2_strings.js
@@ -1,11 +1,19 @@
 const exercise2 = ()=>
     {
-        console.log('Exercise 2');
+        console.log('- Exercise 2 -');
+        console.log("_____________________________");
+
+        const response = "     Hi Zuki! You Have Successfully Completed Exercise 2!     ";
+        console.log("1. Trimmed response:", response.trim());
+        console.log("2. Lowercase response:", response.trim().toLowerCase());
+        console.log("3. Response contains 'success':", response.toLowerCase().includes("success"));
+        console.log("4. Response replaces:", response.trim().replace("Zuki", "Ofentse"))
 
         // Please Complete Exercise 2 here
 
         // End of Exercise 2
-        console.log("Exercise 2 completed");
+        console.log("\n- Exercise 2 completed - ");
+        console.log("______________________________");
     }
 
 module.exports = exercise2;
