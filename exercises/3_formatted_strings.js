edb79e3e4b1f1cd1a98e4617bab8a374120560aa
zukidlomo <zdlomo023@student.wethinkcode.co.za>
2025-05-21 20:19:32 +0200

    Completed exercise 3

--- exercises/3_formatted_strings.js
+++ exercises/3_formatted_strings.js
@@ -1,11 +1,20 @@
 const exercise3 = ()=>
     {
-        console.log('Exercise 3');
+        console.log('- Exercise 3 -');
+        console.log("_____________________________\n");
 
-        // Please Complete Exercise 3 here
+        const testName = "User Registration";
+        const testerName = "Zuki Dlomo"
+        const duration = 5
+        const result = "failed"
+
+        console.log(`Test '${testName}' was executed by ${testerName} and it took them ${duration} hours to complete. The result of the test is :'${result}'.`);
+
+        
 
         // End of Exercise 3
-        console.log("Exercise 3 completed");
+        console.log("\n- Exercise 3 completed -");
+         console.log("_____________________________");
     }
 
 
