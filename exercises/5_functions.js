88f392ae784274b46b75bb1ee2c0dccbdf54f1cb
ZukiDlomo <zdlomo023@student.wethinkcode.co.za>
2025-05-22 10:18:28 +0200

    Exercise 5.1 and 5.2 complete

--- exercises/5_functions.js
+++ exercises/5_functions.js
@@ -1,11 +1,31 @@
 const exercise5 = ()=>
     {
-        console.log('Exercise 5');
+        console.log('- Exercise 5 -');
+        console.log("_____________________________");
 
         // Please Complete Exercise 5 here
+        function logResult(testName, passed){
+            passed ? console.log(`Test "${testName}" passed.`) : console.log(`Test ${testName} failed.`);
+
+        }
+        logResult("User Registration", true);
+
+        function calculateDuration(startTime, endTime){
+            let elaspedTime = endTime - startTime;
+            console.log(`Elasped Time : ${elaspedTime} minutes.`);
+
+        }
+        calculateDuration(10,45);
+
+        function formattedReport(){
+            let report = {
+
+            }
+        }
 
         // End of Exercise 5
-        console.log("Exercise 5 completed");
+        console.log("\n- Exercise 5 completed -");
+        console.log("_____________________________");
     }
 
 
