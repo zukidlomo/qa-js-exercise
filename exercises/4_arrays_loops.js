2638845648470a33dbb65fe0b9e21099f0c1c123
zukidlomo <zdlomo023@student.wethinkcode.co.za>
2025-05-22 05:56:26 +0200

    Attempted exercise 4

--- exercises/4_arrays_loops.js
+++ exercises/4_arrays_loops.js
@@ -1,12 +1,29 @@
 const exercise4 = ()=>
     {
-        console.log('Exercise 4');
+        console.log('- Exercise 4 -');
         console.log("_____________________________");
+
+        let qaSteps = ["Go to www.example.com", "click register button", "enter username: user1", "enter password: pass1", "enter email: example@email.com"];
+        qaSteps.forEach((step, i) => console.log(`Step ${i + 1}: ${step}`));
+
+        console.log("\n");
+        
+        for(i = 0; i < qaSteps.length; i++){
+            console.log(`Executing step ${i+1} ${qaSteps[i]}`);
+        }
+
+        console.log("\n");
+
+        let steps = qaSteps.map(convertUppercase)
+        console.log(steps)
+        function convertUppercase(step){
+            return step.toUpperCase();
+        }
 
         // Please Complete Exercise 4 here
 
         // End of Exercise 4
-        console.log("Exercise 4 completed");
+        console.log("\n- Exercise 4 completed -");
         console.log("_____________________________");
     }   
 
