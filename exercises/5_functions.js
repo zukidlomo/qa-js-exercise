86852e5702accf39c24e30f9758bba45e78bb07f
ZukiDlomo <zdlomo023@student.wethinkcode.co.za>
2025-05-22 11:23:20 +0200

    Completed exercise 5

--- exercises/5_functions.js
+++ exercises/5_functions.js
@@ -3,25 +3,33 @@ const exercise5 = ()=>
         console.log('- Exercise 5 -');
         console.log("_____________________________");
 
-        // Please Complete Exercise 5 here
+        // Exercise 5.1, log passed or failed.
         function logResult(testName, passed){
-            passed ? console.log(`Test "${testName}" passed.`) : console.log(`Test ${testName} failed.`);
-
+            passed === true ? console.log(`Test '${testName}' passed.`) : console.log(`Test ${testName} failed.`);
         }
-        logResult("User Registration", true);
+        logResult("User Registration", false);
 
+        // Exercise 5.2, log elasped time.
         function calculateDuration(startTime, endTime){
             let elaspedTime = endTime - startTime;
-            console.log(`Elasped Time : ${elaspedTime} minutes.`);
-
+            console.log(`\nElasped Time : ${elaspedTime} minutes.`);
         }
         calculateDuration(10,45);
 
+        // Exercise 5.3, log formatted report.
         function formattedReport(){
             let report = {
-
+                title: "Test Report.",
+                testName: "User Registration.",
+                testStatus: "Failed.",
+                expectedResult: "User is able to register successfully.",
+                actualResult: "User was not able to register successfully.",
             }
+           
+            console.log(`\nTitle: ${report.title}\nTest Name: ${report.testName}\nTest Status: ${report.testStatus}\nExpected Result: ${report.expectedResult}\nActual Result: ${report.actualResult}`);
+            return report;
         }
+        formattedReport();
 
         // End of Exercise 5
         console.log("\n- Exercise 5 completed -");
